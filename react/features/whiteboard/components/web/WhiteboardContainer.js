2c3c22a57cc40360401ab6fd1d1cbd2ca4cf7a1f
robertpin <robert.pin9@gmail.com>
2022-03-09 11:15:00 +0200

    Make container respect aspect ratio

--- react/features/whiteboard/components/web/WhiteboardContainer.js
+++ react/features/whiteboard/components/web/WhiteboardContainer.js
@@ -1,27 +1,49 @@
 // @flow
 import { makeStyles } from '@material-ui/core';
-import React from 'react';
+import React, { useMemo } from 'react';
 import { useSelector } from 'react-redux';
 
 import { getVerticalViewMaxWidth } from '../../../filmstrip/functions.web';
+import { WHITEBOARD_ASPECT_RATIO } from '../../constants';
 
 const useStyles = makeStyles(() => {
     return {
         container: {
             position: 'absolute',
             backgroundColor: '#fff',
-            top: 0,
-            width: '100%',
-            height: '100%'
+            top: 0
         }
     };
 });
 
-const WhiteboardContainer = props => {
+const WhiteboardContainer = () => {
     const styles = useStyles();
     const filmstripWidth = useSelector(getVerticalViewMaxWidth);
+    const { clientHeight, clientWidth } = useSelector(state => state['features/base/responsive-ui']);
+    const dimensions = useMemo(() => {
+        const maxWidth = clientWidth - filmstripWidth;
+        const aspectRatioHeight = maxWidth / WHITEBOARD_ASPECT_RATIO;
 
-    return <div className = { styles.container }>yello</div>;
+        if (aspectRatioHeight <= clientHeight) {
+            return {
+                width: maxWidth,
+                height: aspectRatioHeight,
+                left: 0,
+                top: (clientHeight - aspectRatioHeight) / 2
+            };
+        }
+
+        return {
+            height: clientHeight,
+            width: clientHeight * WHITEBOARD_ASPECT_RATIO,
+            top: 0,
+            left: (maxWidth - (clientHeight * WHITEBOARD_ASPECT_RATIO)) / 2
+        };
+    }, [ filmstripWidth, clientHeight, clientWidth ]);
+
+    return (<div
+        className = { styles.container }
+        style = { dimensions }>yello</div>);
 };
 
 export default WhiteboardContainer;
---
+++ react/features/whiteboard/constants.js
@@ -0,0 +1,4 @@
+/**
+ * The aspect ratio of the whiteboard.
+ */
+export const WHITEBOARD_ASPECT_RATIO = 16 / 9;
